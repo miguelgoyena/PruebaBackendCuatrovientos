cf9123c6bf5919b72dbdc218ff85ef50d2724550
Miguel Goyena <miguel.goyena@davalorsalud.com>
2018-01-19 12:38:34 +0100

    Anadiendo superpom y ya aplicacion completa

--- controller/src/main/java/com/bizgenco/backend/controller/App.java
+++ controller/src/main/java/com/bizgenco/backend/controller/App.java
@@ -1,15 +1,12 @@
 package com.bizgenco.backend.controller;
 
-import java.util.Arrays;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+
 
 /**
- * Hello world!
+ * Backend ERP Application
  * from https://spring.io/guides/gs/spring-boot/
  */
 @SpringBootApplication
@@ -19,19 +16,5 @@ public class App
     {
     	SpringApplication.run(App.class, args);
     }
-    
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
 
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-        };
-    }
 }
